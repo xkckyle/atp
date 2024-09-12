34e1bed7ce4ba5a0bf0f7eb0e81aa9b0234fe708
xkckyle <k-c@pacbell.net>
2024-09-11 23:23:45 -0400

    woooo

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-### 
-### 
-### 
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
 
-# INCLUDES
+df = pd.read_csv('January 2024 to Current Day.csv')
+df.columns = df.columns.str.strip()
+df.columns = df.columns.str.replace(r'\n','',regex=True)
+df.columns = df.columns.str.replace(r'\r','',regex=True)
+df.columns = df.columns.str.replace(r'\t','',regex=True)
+df.columns = df.columns.str.replace(' ','_',regex=True)
+
+df[['Chemical','Bath']] = df['Phase'].str.split(' ', n=1, expand=True)
+
+
+# df.to_csv('atp.csv')
 
 
 
-# VARS
 
 
-
-# ...
-print('hello')
+#
+print(
+    df['Chemical'].unique()
+      )
+# c = df.groupby('Chemical')['Chemical'].nunique()
+# print ( 
+#     c
+    
+#     )
+# print (pd.DataFrame.from_records(c.values.tolist()).stack().value_counts())
